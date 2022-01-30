2eaf7a1b0bee06cecdf2535b55f046551a976ac9
Devesh <deveshsachdev97@gmail.com>
2022-01-30 23:18:42 +0530

    add products controllers

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require('dotenv').config();
+const errorHandler = require('./middleware/error-handler')
+const notFound = require('./middleware/not-found')
+const connectDB = require('./db/connect')
+const express = require('express');
+const productsRouter = require('./routes/products')
+
+const app = express();
+app.use(express.json());
+
+
+//routes
+app.get('/',(req,res)=>{
+  res.send('you have hit home route')
+})
+
+app.use('/api/v1/products',productsRouter)
+
+app.use(notFound)
+app.use(errorHandler)
+
+const port = process.env.PORT || 3000
+
+const start = async ()=>{
+  try{
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port,console.log(`server running on ${port}`))
+  }catch(err){
+    console.log(err)
+  }
+}
+
+start()
